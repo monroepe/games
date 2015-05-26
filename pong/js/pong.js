2d1e149019ae30720953815290f1f9837d6c3d6a
Peter <monroepe@gmail.com>
2015-05-26 17:36:14 -0400

    Paddles move up and down now

--- pong/js/pong.js
+++ pong/js/pong.js
@@ -1,4 +1,29 @@
+var KEY= {
+    UP: 38,
+    DOWN: 40,
+    W: 87,
+    S: 83
+}
+
 $(function(){
-  $("#paddleB").css("top", "20px");
-  $("#paddleA").css("top", "60px");
+  $(document).keydown(function(e) {
+    switch(e.which) {
+      case KEY.UP:
+        var top = parseInt($("#paddleB").css("top"));
+        $("#paddleB").css("top",top-5);
+        break;
+      case KEY.DOWN:
+        var top = parseInt($("#paddleB").css("top"));
+        $("#paddleB").css("top",top+5);
+        break;
+      case KEY.W:
+        var top = parseInt($("#paddleA").css("top"));
+        $("#paddleA").css("top",top-5);
+        break;
+      case KEY.S:
+        var top = parseInt($("#paddleA").css("top"));
+        $("#paddleA").css("top",top+5);
+        break;
+    }
+  })
 });
